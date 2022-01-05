673327753a169293c54e64ddb9bc3f4e4c1412f6
AlexeyDyvinets <oleksii.dyvinets@nixsolutions.com>
2022-01-05 10:41:49 +0200

    first commit

---
+++ GUI/main.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("internal_but");
+
+btn.onclick = function () {
+    eel.login_py()
+}
---
+++ auth_data.py
@@ -0,0 +1,5 @@
+# Org IDs
+internal_dev_org_id = "us-east-1_UxvyI5D7I"
+admin_userName = "lydyia.en@nixsolutions.com"
+password = "Qwerty123*"
+
