72b536a72f8f10d8eaa254a07d244d42fabdd96d
AlexeyDyvinets <oleksii.dyvinets@nixsolutions.com>
2022-01-05 12:23:40 +0200

    dev logins

--- GUI/main.js
+++ GUI/main.js
@@ -1,5 +1,19 @@
-let btn = document.getElementById("internal_but");
+let dev_int_btn = document.getElementById("dev_internal_but").onclick
+= function dev_int_login() {
+    eel.dev_int_log()
+}
 
-btn.onclick = function () {
-    eel.login_py()
+let dev_ass_btn = document.getElementById("dev_asset_but").onclick
+= function dev_ass_login() {
+    eel.dev_ass_log()
+}
+
+let dev_cash_btn = document.getElementById("dev_cash_but").onclick
+= function dev_cash_login() {
+    eel.dev_cash_log()
+}
+
+let dev_fiig_btn = document.getElementById("dev_fiig_but").onclick
+= function dev_fiig_login() {
+    eel.dev_fiig_log()
 }
--- login.py
+++ login.py
@@ -8,13 +8,12 @@ eel.init("GUI")
 
 
 @eel.expose
-def login_py():
+def dev_int_log():
     options = Options()
     options.add_argument('start-maximized')
     options.add_argument('disable-infobars')
     global driver
     driver = webdriver.Chrome(chrome_options = options, executable_path="C:\\Users\\dyvinets\\Desktop\\Autotest\\chromeDriver\\chromedriver.exe")
-    url = "https://dev.bonditplatform.com/auth"
     driver.get(url=url)
     orgIDField = driver.find_element_by_id('client_id')
     orgIDField.clear()
@@ -26,5 +25,61 @@ def login_py():
     password_field = driver.find_element_by_id("user_password").send_keys(password)
     login_button = driver.find_element_by_xpath("/html/body/app-root/app-login/div/div/div/div/div[5]/input").click()
     time.sleep(10)
-    
+
+@eel.expose
+def dev_ass_log():
+    options = Options()
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    global driver
+    driver = webdriver.Chrome(chrome_options = options, executable_path="C:\\Users\\dyvinets\\Desktop\\Autotest\\chromeDriver\\chromedriver.exe")
+    driver.get(url=url)
+    orgIDField = driver.find_element_by_id('client_id')
+    orgIDField.clear()
+    orgIDField.send_keys(asset_dev_org_id)
+    time.sleep(2)
+    nextButton = driver.find_element_by_xpath('/html/body/app-root/org-field/div/div/div/div/div[2]/input[2]').click()
+    time.sleep(2)
+    username_field = driver.find_element_by_id("user_name").send_keys(regular_userName)
+    password_field = driver.find_element_by_id("user_password").send_keys(password)
+    login_button = driver.find_element_by_xpath("/html/body/app-root/app-login/div/div/div/div/div[5]/input").click()
+    time.sleep(10)
+
+@eel.expose
+def dev_cash_log():
+    options = Options()
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    global driver
+    driver = webdriver.Chrome(chrome_options = options, executable_path="C:\\Users\\dyvinets\\Desktop\\Autotest\\chromeDriver\\chromedriver.exe")
+    driver.get(url=url)
+    orgIDField = driver.find_element_by_id('client_id')
+    orgIDField.clear()
+    orgIDField.send_keys(cashwerkz_dev_org_id)
+    time.sleep(2)
+    nextButton = driver.find_element_by_xpath('/html/body/app-root/org-field/div/div/div/div/div[2]/input[2]').click()
+    time.sleep(2)
+    username_field = driver.find_element_by_id("user_name").send_keys(admin_userName)
+    password_field = driver.find_element_by_id("user_password").send_keys(password)
+    login_button = driver.find_element_by_xpath("/html/body/app-root/app-login/div/div/div/div/div[5]/input").click()
+    time.sleep(10)
+
+@eel.expose
+def dev_fiig_log():
+    options = Options()
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    global driver
+    driver = webdriver.Chrome(chrome_options = options, executable_path="C:\\Users\\dyvinets\\Desktop\\Autotest\\chromeDriver\\chromedriver.exe")
+    driver.get(url=url)
+    orgIDField = driver.find_element_by_id('client_id')
+    orgIDField.clear()
+    orgIDField.send_keys(fiig_dev_org_id)
+    time.sleep(2)
+    nextButton = driver.find_element_by_xpath('/html/body/app-root/org-field/div/div/div/div/div[2]/input[2]').click()
+    time.sleep(2)
+    username_field = driver.find_element_by_id("user_name").send_keys(admin_userName)
+    password_field = driver.find_element_by_id("user_password").send_keys(password)
+    login_button = driver.find_element_by_xpath("/html/body/app-root/app-login/div/div/div/div/div[5]/input").click()
+    time.sleep(10)
 eel.start("main.html")
